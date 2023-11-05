57062c0b65c85bb3589a70a9d6fa430f6751d2cd
Hitesh Choudhary <hitesh@hiteshchoudhary.com>
2023-11-05 05:27:28 +0000

    string discussion

---
+++ basics1/conversion.js
@@ -0,0 +1,10 @@
+let bankBalance = 'abc'
+
+let bankBalanceinInt = Number(bankBalance)
+
+console.log( bankBalance);
+console.log(typeof bankBalanceinInt);
+
+let c = null
+console.log( 5 + undefined);
+console.log( 5 + null);
---
+++ basics1/numAndString.js
@@ -0,0 +1,19 @@
+const firstName = 'Hello'
+const lastName = "HC"
+
+console.log(firstName + ' ' + lastName);
+console.log(`my firstname ${firstName} is and my lastname is ${lastName}`);
+
+
+const username = new String('hiteshdotcom')
+console.log(username[0]);
+console.log(username.length);
+console.log(username.charAt(0));
+console.log(username.substring(0, 4));
+
+let email = " h@hc.com     "
+console.log(email.trim());
+let emailTwo = "hc@pw.live"
+console.log(emailTwo.replace('hc', 'hitesh'));
+console.log(email.includes('hc'));
+console.log(emailTwo.toUpperCase());
