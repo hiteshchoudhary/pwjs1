968cda386cbe6c49f2e916c857a5cf853b78b84b
Hitesh Choudhary <hitesh@hiteshchoudhary.com>
2023-11-19 05:21:06 +0000

    logical operations

--- control2/01-if.js
+++ control2/01-if.js
@@ -96,3 +96,75 @@ if (starInNumber == 1) {
 // short hand syntax
 // if(balance > 10) console.log("IF part");
 // else console.log("Else part");
+
+
+// login system
+
+// const email = ""
+
+// if email is present, ask for password. Otherwise ask for email
+
+// if (email ){
+//     console.log("please enter your password");
+// } else {
+//     console.log("Please enter your email");
+// }
+
+// if (email != "") {
+//     console.log("Please provide a password");
+// } else {
+//     console.log("Please enter your email");
+// }
+
+console.log(Boolean(-1));
+
+// truthy falsy
+
+// falsy values
+
+/*
+
+false
+0           (-0 and BigInt 0n)
+""
+null
+undefined
+NaN
+
+
+*/
+
+
+// if user provides email, password, then allow login, else ask for it
+
+const email = ""
+const password = ""
+
+if(email){
+    if (password) {
+        console.log("welcome to the app");
+    }else{
+        console.log("please enter password");
+    }
+}else {
+    console.log("please enter your email");
+}
+
+// Logical Operation
+// && -> and ; || => OR
+
+if (email && password) {
+    console.log("welcome to the app");
+}else {
+    console.log("Please enter email and password");
+}
+
+// allow user to signin with google or github
+const googleLoginToken = ""
+const githubLoginToken = ""
+
+if (googleLoginToken || githubLoginToken) {
+    console.log("Welcome to the app");
+}else {
+    console.log("Please login to view your account");
+}
